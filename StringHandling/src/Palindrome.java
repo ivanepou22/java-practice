e992af3a29aeeb1b88746145731f86a7150dffa5
Ivan Epou <ivanepou92@gmail.com>
2023-03-19 20:39:44 +0300

    Update the palindrome to cater for cases

--- StringHandling/src/Palindrome.java
+++ StringHandling/src/Palindrome.java
@@ -1,10 +1,15 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String x  = "tet";
+        String x  = "too Hot to HOOT";
+        //too hot to hoot or too Hot to HOOT
         System.out.println(palindromeString(x));
     }
 
     private static Boolean palindromeString(String x) {
+        //remove spaces in the string
+        x = x.replace(" ", "");
+        //convert to lower case.
+        x = x.toLowerCase();
         char[] charString = x.toCharArray();
         int size = charString.length;
         char[] a = new char[size];
