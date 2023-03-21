55ac2d9c82995cdcf40cd8830b72bdb04d588412
Ivan Epou <ivanepou92@gmail.com>
2023-03-21 22:22:06 +0300

    Check if a string is empty or not

---
+++ StringHandling/src/CheckIfAStringIsEmptyOrNot.java
@@ -0,0 +1,10 @@
+public class CheckIfAStringIsEmptyOrNot {
+    public static void main(String[] args) {
+        String x = "";
+        if (x.length() == 0){
+            System.out.println("String is empty");
+        } else {
+            System.out.println("String is not empty");
+        }
+    }
+}
