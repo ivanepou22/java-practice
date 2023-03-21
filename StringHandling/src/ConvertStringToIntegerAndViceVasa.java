b12ff9f2846acbf361927b936a3a7923370c08bf
Ivan Epou <ivanepou92@gmail.com>
2023-03-22 00:08:39 +0300

    Convert a String To Integer And ViceVasa

---
+++ StringHandling/src/ConvertStringToIntegerAndViceVasa.java
@@ -0,0 +1,13 @@
+public class ConvertStringToIntegerAndViceVasa {
+    public static void main(String[] args) {
+        //Integer to String
+        int x = 65;
+        String xString = Integer.toString(x);
+        System.out.println(xString);
+
+        //String to Integer
+        String y = "65";
+        int yInteger = Integer.parseInt(y);
+        System.out.println(yInteger);
+    }
+}
