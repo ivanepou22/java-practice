38c7cfcaba80e664a0fec3b5b211fe42061deda9
Ivan Epou <ivanepou92@gmail.com>
2023-03-28 21:00:44 +0300

    Use switch statement

---
+++ SampleCodingExercises/src/SwitchStatement.java
@@ -0,0 +1,17 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int switchValue = 1;
+
+        System.out.println(getMonth("MAY")+ " quarter");
+    }
+
+    public static String getMonth(String month) {
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> "Bad";
+        };
+    }
+}
